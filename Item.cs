2b26a1a742bd2a9e2e964a20f3f1eeaefc8a385c
Patreskoo1 <patrikcernak35@gmail.com>
2026-03-23 15:26:18 +0100

    Change Type property to use ItemType enum

--- Item.cs
+++ Item.cs
@@ -1,7 +1,15 @@
 public class Item
 {
     public string? Name {get; set; }
-    public string? Type {get; set; }
+    public ItemType Type {get; set; }
     public int Value {get; set; }
     public int Price {get; set; }
+}
+
+public enum ItemType
+{
+    Weapon,
+    Armor,
+    Consumable,
+    Accessory
 }
