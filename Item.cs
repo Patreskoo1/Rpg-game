3557cc2a8060efdffb583dbd6195ebdfebb09ad9
Patreskoo1 <patrikcernak35@gmail.com>
2026-04-02 16:58:59 +0200

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -1,9 +1,21 @@
 public class Item
 {
     public string? Name {get; set; }
+    public string? Description {get; set; }
     public ItemType Type {get; set; }
     public int Value {get; set; }
     public int Price {get; set; }
+
+    public Item() { }
+
+    public Item(string name, string description, ItemType type, int value, int price)
+    {
+        Name = name;
+        Description = description;
+        Type = type;
+        Value = value;
+        Price = price;
+    }
 }
 
 public enum ItemType
